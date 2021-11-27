88230159b2152da2ed02b79851649f2fcb62da7b
KgalabiSimon <54315034@mylife.unisa.ac.za>
2021-11-27 11:26:08 +0200

    added a customer model in users/models.py added admin.site.register(customer) in users/admin.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Customer(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
