79ce7c90e22a3b95d15d40b0cc7bbee487f62578
KgalabiSimon <54315034@mylife.unisa.ac.za>
2021-11-28 21:48:23 +0200

    redefine Customer model in users/models.py

--- users/models.py
+++ users/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
 
 class Customer(models.Model):
-    customer = models.ForeignKey(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=255)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+
